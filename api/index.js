12a4d8409f05b2e6c713800a84256135da5f578c
Richard Pires <richardjpires@gmail.com>
2023-04-27 20:00:14 -0400

    remove speech button if unsupported. moved speech buttons inline with input. moved reset button in line with submit button.

--- api/index.js
+++ api/index.js
@@ -417,106 +417,22 @@ function Input() {
   function reset() {
     setQuery(""), setSpeechInput(""), setResponse(""), resetTranscript(), import_react_speech_recognition.default.stopListening(), setError("");
   }
-  return isReady ? browserSupportsSpeechRecognition ? /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)("div", { className: "input-component", children: [
+  return isReady ? /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)("div", { className: "input-component", children: [
     /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)("h2", { children: [
       "say or type a word/phrase/whatever,",
       /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)("br", {}, void 0, !1, {
         fileName: "app/components/input.js",
-        lineNumber: 77,
+        lineNumber: 73,
         columnNumber: 9
       }, this),
       "get a definition and example."
     ] }, void 0, !0, {
       fileName: "app/components/input.js",
-      lineNumber: 75,
+      lineNumber: 71,
       columnNumber: 7
     }, this),
-    /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)("form", { onSubmit, children: [
-      /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)("div", { className: "buttons", children: [
-        !listening && /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)("div", { className: "button-container", children: [
-          /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)(
-            "button",
-            {
-              className: "start-microphone",
-              type: "button",
-              tabIndex: "-1",
-              onClick: import_react_speech_recognition.default.startListening
-            },
-            void 0,
-            !1,
-            {
-              fileName: "app/components/input.js",
-              lineNumber: 87,
-              columnNumber: 15
-            },
-            this
-          ),
-          /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)("svg", { xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 384 512", children: /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)("path", { d: "M192 0C139 0 96 43 96 96V256c0 53 43 96 96 96s96-43 96-96V96c0-53-43-96-96-96zM64 216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 89.1 66.2 162.7 152 174.4V464H120c-13.3 0-24 10.7-24 24s10.7 24 24 24h72 72c13.3 0 24-10.7 24-24s-10.7-24-24-24H216V430.4c85.8-11.7 152-85.3 152-174.4V216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 70.7-57.3 128-128 128s-128-57.3-128-128V216z" }, void 0, !1, {
-            fileName: "app/components/input.js",
-            lineNumber: 95,
-            columnNumber: 17
-          }, this) }, void 0, !1, {
-            fileName: "app/components/input.js",
-            lineNumber: 94,
-            columnNumber: 15
-          }, this)
-        ] }, void 0, !0, {
-          fileName: "app/components/input.js",
-          lineNumber: 86,
-          columnNumber: 13
-        }, this),
-        listening && /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)("div", { className: "button-container", children: [
-          /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)(
-            "button",
-            {
-              className: "stop-microphone",
-              type: "button",
-              tabIndex: "-1",
-              onClick: import_react_speech_recognition.default.stopListening
-            },
-            void 0,
-            !1,
-            {
-              fileName: "app/components/input.js",
-              lineNumber: 103,
-              columnNumber: 15
-            },
-            this
-          ),
-          /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)("svg", { xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 640 512", children: /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)("path", { d: "M38.8 5.1C28.4-3.1 13.3-1.2 5.1 9.2S-1.2 34.7 9.2 42.9l592 464c10.4 8.2 25.5 6.3 33.7-4.1s6.3-25.5-4.1-33.7L472.1 344.7c15.2-26 23.9-56.3 23.9-88.7V216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 21.2-5.1 41.1-14.2 58.7L416 300.8V96c0-53-43-96-96-96s-96 43-96 96v54.3L38.8 5.1zM344 430.4c20.4-2.8 39.7-9.1 57.3-18.2l-43.1-33.9C346.1 382 333.3 384 320 384c-70.7 0-128-57.3-128-128v-8.7L144.7 210c-.5 1.9-.7 3.9-.7 6v40c0 89.1 66.2 162.7 152 174.4V464H248c-13.3 0-24 10.7-24 24s10.7 24 24 24h72 72c13.3 0 24-10.7 24-24s-10.7-24-24-24H344V430.4z" }, void 0, !1, {
-            fileName: "app/components/input.js",
-            lineNumber: 112,
-            columnNumber: 17
-          }, this) }, void 0, !1, {
-            fileName: "app/components/input.js",
-            lineNumber: 111,
-            columnNumber: 15
-          }, this)
-        ] }, void 0, !0, {
-          fileName: "app/components/input.js",
-          lineNumber: 102,
-          columnNumber: 13
-        }, this),
-        /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)("div", { className: "button-container", children: /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)("button", { type: "button", tabIndex: "-1", onClick: reset, disabled: !speechInput, children: "reset" }, void 0, !1, {
-          fileName: "app/components/input.js",
-          lineNumber: 121,
-          columnNumber: 13
-        }, this) }, void 0, !1, {
-          fileName: "app/components/input.js",
-          lineNumber: 120,
-          columnNumber: 11
-        }, this)
-      ] }, void 0, !0, {
-        fileName: "app/components/input.js",
-        lineNumber: 83,
-        columnNumber: 9
-      }, this),
-      listening && /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)("div", { className: "listening", children: "listening..." }, void 0, !1, {
-        fileName: "app/components/input.js",
-        lineNumber: 127,
-        columnNumber: 23
-      }, this),
-      /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)("div", { className: "input-container", children: [
+    /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)("form", { onSubmit, children: /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)("div", { className: "input-container", children: [
+      /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)("div", { className: "row", children: [
         /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)(
           "input",
           {
@@ -531,11 +447,86 @@ function Input() {
           !1,
           {
             fileName: "app/components/input.js",
-            lineNumber: 131,
-            columnNumber: 11
+            lineNumber: 82,
+            columnNumber: 13
           },
           this
         ),
+        !listening && browserSupportsSpeechRecognition && /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)("div", { className: "button-container", children: [
+          /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)(
+            "button",
+            {
+              className: "start-microphone",
+              type: "button",
+              tabIndex: "-1",
+              onClick: import_react_speech_recognition.default.startListening
+            },
+            void 0,
+            !1,
+            {
+              fileName: "app/components/input.js",
+              lineNumber: 94,
+              columnNumber: 17
+            },
+            this
+          ),
+          /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)("svg", { xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 384 512", children: /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)("path", { d: "M192 0C139 0 96 43 96 96V256c0 53 43 96 96 96s96-43 96-96V96c0-53-43-96-96-96zM64 216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 89.1 66.2 162.7 152 174.4V464H120c-13.3 0-24 10.7-24 24s10.7 24 24 24h72 72c13.3 0 24-10.7 24-24s-10.7-24-24-24H216V430.4c85.8-11.7 152-85.3 152-174.4V216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 70.7-57.3 128-128 128s-128-57.3-128-128V216z" }, void 0, !1, {
+            fileName: "app/components/input.js",
+            lineNumber: 102,
+            columnNumber: 19
+          }, this) }, void 0, !1, {
+            fileName: "app/components/input.js",
+            lineNumber: 101,
+            columnNumber: 17
+          }, this)
+        ] }, void 0, !0, {
+          fileName: "app/components/input.js",
+          lineNumber: 93,
+          columnNumber: 15
+        }, this),
+        listening && browserSupportsSpeechRecognition && /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)("div", { className: "button-container", children: [
+          /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)(
+            "button",
+            {
+              className: "stop-microphone",
+              type: "button",
+              tabIndex: "-1",
+              onClick: import_react_speech_recognition.default.stopListening
+            },
+            void 0,
+            !1,
+            {
+              fileName: "app/components/input.js",
+              lineNumber: 110,
+              columnNumber: 17
+            },
+            this
+          ),
+          /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)("svg", { xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 640 512", children: /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)("path", { d: "M38.8 5.1C28.4-3.1 13.3-1.2 5.1 9.2S-1.2 34.7 9.2 42.9l592 464c10.4 8.2 25.5 6.3 33.7-4.1s6.3-25.5-4.1-33.7L472.1 344.7c15.2-26 23.9-56.3 23.9-88.7V216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 21.2-5.1 41.1-14.2 58.7L416 300.8V96c0-53-43-96-96-96s-96 43-96 96v54.3L38.8 5.1zM344 430.4c20.4-2.8 39.7-9.1 57.3-18.2l-43.1-33.9C346.1 382 333.3 384 320 384c-70.7 0-128-57.3-128-128v-8.7L144.7 210c-.5 1.9-.7 3.9-.7 6v40c0 89.1 66.2 162.7 152 174.4V464H248c-13.3 0-24 10.7-24 24s10.7 24 24 24h72 72c13.3 0 24-10.7 24-24s-10.7-24-24-24H344V430.4z" }, void 0, !1, {
+            fileName: "app/components/input.js",
+            lineNumber: 119,
+            columnNumber: 19
+          }, this) }, void 0, !1, {
+            fileName: "app/components/input.js",
+            lineNumber: 118,
+            columnNumber: 17
+          }, this)
+        ] }, void 0, !0, {
+          fileName: "app/components/input.js",
+          lineNumber: 109,
+          columnNumber: 15
+        }, this)
+      ] }, void 0, !0, {
+        fileName: "app/components/input.js",
+        lineNumber: 80,
+        columnNumber: 11
+      }, this),
+      listening && /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)("div", { className: "listening", children: "listening..." }, void 0, !1, {
+        fileName: "app/components/input.js",
+        lineNumber: 125,
+        columnNumber: 25
+      }, this),
+      /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)("div", { className: "buttons", children: [
         /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)(
           "button",
           {
@@ -549,56 +540,61 @@ function Input() {
           !1,
           {
             fileName: "app/components/input.js",
-            lineNumber: 141,
-            columnNumber: 11
-          },
-          this
-        ),
-        error && /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)(
-          "div",
-          {
-            className: "error",
-            dangerouslySetInnerHTML: {
-              __html: import_dompurify3.default.sanitize(error)
-            }
-          },
-          void 0,
-          !1,
-          {
-            fileName: "app/components/input.js",
-            lineNumber: 152,
+            lineNumber: 129,
             columnNumber: 13
           },
           this
-        )
+        ),
+        /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)("button", { type: "button", tabIndex: "-1", onClick: reset, disabled: !speechInput, children: "reset" }, void 0, !1, {
+          fileName: "app/components/input.js",
+          lineNumber: 139,
+          columnNumber: 13
+        }, this)
       ] }, void 0, !0, {
         fileName: "app/components/input.js",
-        lineNumber: 129,
-        columnNumber: 9
-      }, this)
+        lineNumber: 127,
+        columnNumber: 11
+      }, this),
+      error && /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)(
+        "div",
+        {
+          className: "error",
+          dangerouslySetInnerHTML: {
+            __html: import_dompurify3.default.sanitize(error)
+          }
+        },
+        void 0,
+        !1,
+        {
+          fileName: "app/components/input.js",
+          lineNumber: 146,
+          columnNumber: 13
+        },
+        this
+      )
     ] }, void 0, !0, {
       fileName: "app/components/input.js",
-      lineNumber: 82,
+      lineNumber: 79,
+      columnNumber: 9
+    }, this) }, void 0, !1, {
+      fileName: "app/components/input.js",
+      lineNumber: 78,
       columnNumber: 7
     }, this),
     isLoading && /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)(import_react_spinners.PropagateLoader, { color: "#005277", className: "loader" }, void 0, !1, {
       fileName: "app/components/input.js",
-      lineNumber: 163,
+      lineNumber: 157,
       columnNumber: 21
     }, this),
     response && /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)(Response2, { data: response, query }, void 0, !1, {
       fileName: "app/components/input.js",
-      lineNumber: 166,
+      lineNumber: 160,
       columnNumber: 20
     }, this)
   ] }, void 0, !0, {
     fileName: "app/components/input.js",
-    lineNumber: 74,
-    columnNumber: 5
-  }, this) : /* @__PURE__ */ (0, import_jsx_dev_runtime4.jsxDEV)("span", { children: "Browser doesn't support speech recognition." }, void 0, !1, {
-    fileName: "app/components/input.js",
     lineNumber: 70,
-    columnNumber: 12
+    columnNumber: 5
   }, this) : null;
 }
 var input_default = Input;
@@ -607,7 +603,7 @@ var input_default = Input;
 var styles_default = "/build/_assets/index-DUDNNKNR.css";
 
 // app/styles/input.css
-var input_default2 = "/build/_assets/input-NBPD6YXT.css";
+var input_default2 = "/build/_assets/input-SLJ6PGXN.css";
 
 // app/routes/index.jsx
 var import_react7 = require("react"), import_jsx_dev_runtime5 = require("react/jsx-dev-runtime");
@@ -677,7 +673,7 @@ function Index() {
 }
 
 // server-assets-manifest:@remix-run/dev/assets-manifest
-var assets_manifest_default = { version: "b35feeb6", entry: { module: "/build/entry.client-HP5ZXVQM.js", imports: ["/build/_shared/chunk-DEMPFQNT.js", "/build/_shared/chunk-FN3KWL4V.js", "/build/_shared/chunk-4IYZMDEG.js"] }, routes: { root: { id: "root", parentId: void 0, path: "", index: void 0, caseSensitive: void 0, module: "/build/root-DSAQHILR.js", imports: void 0, hasAction: !1, hasLoader: !0, hasCatchBoundary: !1, hasErrorBoundary: !1 }, "routes/index": { id: "routes/index", parentId: "root", path: void 0, index: !0, caseSensitive: void 0, module: "/build/routes/index-2HXDMQY7.js", imports: void 0, hasAction: !1, hasLoader: !1, hasCatchBoundary: !1, hasErrorBoundary: !1 }, "routes/resources/manifest[.]webmanifest": { id: "routes/resources/manifest[.]webmanifest", parentId: "root", path: "resources/manifest.webmanifest", index: void 0, caseSensitive: void 0, module: "/build/routes/resources/manifest[.]webmanifest-B7J2LRZ4.js", imports: void 0, hasAction: !1, hasLoader: !0, hasCatchBoundary: !1, hasErrorBoundary: !1 } }, cssBundleHref: void 0, hmr: void 0, url: "/build/manifest-B35FEEB6.js" };
+var assets_manifest_default = { version: "bdbbad8b", entry: { module: "/build/entry.client-HP5ZXVQM.js", imports: ["/build/_shared/chunk-DEMPFQNT.js", "/build/_shared/chunk-FN3KWL4V.js", "/build/_shared/chunk-4IYZMDEG.js"] }, routes: { root: { id: "root", parentId: void 0, path: "", index: void 0, caseSensitive: void 0, module: "/build/root-DSAQHILR.js", imports: void 0, hasAction: !1, hasLoader: !0, hasCatchBoundary: !1, hasErrorBoundary: !1 }, "routes/index": { id: "routes/index", parentId: "root", path: void 0, index: !0, caseSensitive: void 0, module: "/build/routes/index-BSQECNGK.js", imports: void 0, hasAction: !1, hasLoader: !1, hasCatchBoundary: !1, hasErrorBoundary: !1 }, "routes/resources/manifest[.]webmanifest": { id: "routes/resources/manifest[.]webmanifest", parentId: "root", path: "resources/manifest.webmanifest", index: void 0, caseSensitive: void 0, module: "/build/routes/resources/manifest[.]webmanifest-B7J2LRZ4.js", imports: void 0, hasAction: !1, hasLoader: !0, hasCatchBoundary: !1, hasErrorBoundary: !1 } }, cssBundleHref: void 0, hmr: void 0, url: "/build/manifest-BDBBAD8B.js" };
 
 // server-entry-module:@remix-run/dev/server-build
 var assetsBuildDirectory = "public\\build", future = { unstable_cssModules: !1, unstable_cssSideEffectImports: !1, unstable_dev: !1, unstable_postcss: !1, unstable_tailwind: !1, unstable_vanillaExtract: !1, v2_errorBoundary: !1, v2_meta: !1, v2_routeConvention: !1 }, publicPath = "/build/", entry = { module: entry_server_exports }, routes = {
